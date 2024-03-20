e09e939e2998f9dc61bf745660c4a1788b414445
Abhishek Dhakad <dhakaddeepak9340700360@gmail.com>
2024-03-21 00:28:21 +0530

    about us page layout create

--- src/app/pages/main/about/about.component.ts
+++ src/app/pages/main/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+declare var jQuery: any;
+import { isPlatformBrowser } from '@angular/common';
 import { SchoolService } from 'src/app/services/school.service';
 
 @Component({
@@ -7,22 +9,59 @@ import { SchoolService } from 'src/app/services/school.service';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  private isBrowser: boolean = isPlatformBrowser(this.platformId);
   schoolInfo: any;
   loader: Boolean = true;
-  constructor(private schoolService: SchoolService) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,private schoolService: SchoolService) { }
 
   ngOnInit(): void {
     this.getSchool();
   }
   getSchool() {
+    this.loader = false;
     this.schoolService.getSchool().subscribe((res: any) => {
       if (res) {
         this.schoolInfo = res;
         setTimeout(() => {
-          this.loader = false;
+          // this.loader = false;
         }, 1000)
       }
     })
   }
+  ngAfterViewInit() {
+    if (this.isBrowser) {
+
+      setTimeout(() => {
+        
+        jQuery('.award-carousel').owlCarousel({
+          items: 2,
+          loop: true,
+          dots: false,
+          nav: true,
+          autoplay: true,
+
+          autoplayHoverPause: true,
+          responsiveClass: true,
+
+          smartSpeed: 10000,
+          singleItem: false,
+          animateIn: 'fadeIn',
+          animateOut: 'fadeOut',
+          pagination: true,
+          responsive: {
+            600: {
+              items: 6,
+            },
+            1500: {
+              items: 6,
+            },
+          }
+        });
+        setTimeout(() => {
+          this.loader = false;
+        }, 500)
+      }, 3000);
+    }
+    }
 
 }
